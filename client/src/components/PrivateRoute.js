a3a15a18713d6ba9340d9b1ad53f86fab9b3c68d
Priyanshi Maheshwari <priyanshimaheshwari2015@gmail.com>
2023-07-23 00:13:38 +0530

    Revert "Initial Commit"
    This reverts commit a37e6eececb515145b8cd1d4fe2fb4dc8829b36b.

--- client/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoute = ({ children }) => {
-  // const userData = localStorage.getItem("blog-user");
-  const isAuthenticated = false; // Replace with actual authentication check
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
